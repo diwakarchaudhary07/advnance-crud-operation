ded5cad732d099a5121449ede8e966aacbd31a8c
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-09 22:33:03 +0530

    add

--- crud_operation/advance_crud/forms.py
+++ crud_operation/advance_crud/forms.py
@@ -4,4 +4,4 @@ from .models import Student
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
-        fields = ['name', 'email', 'age']
+        fields = ['name', 'email', 'age','city','contact_no']
--- crud_operation/advance_crud/models.py
+++ crud_operation/advance_crud/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+
 class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     age = models.IntegerField()
-
+    city = models.CharField(max_length=100)
+    contact_no = models.CharField(max_length=10)
+    
     def __str__(self):
         return self.name
