701369e0e54bbe029b25a4788015fbd034f42912
diwakar chaudhary <boyroyal4853@gmail.com>
2026-05-11 23:47:04 +0530

    add

--- crud_operation/advance_crud/models.py
+++ crud_operation/advance_crud/models.py
@@ -3,15 +3,21 @@ from django.db import models
 
 class Student(models.Model):
 
-    name = models.CharField(max_length=100,blank=True,null=True)
+    name = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )
 
     email = models.EmailField(blank=True,null=True)
 
-    age = models.IntegerField(blank=True,null=True)
+    age = models.IntegerField( blank=True,null=True)
 
-    city = models.CharField(max_length=100,blank=True,null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
 
     contact_no = models.CharField(max_length=10,blank=True,null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True,blank=True,null=True)
 
     def __str__(self):
         return self.name if self.name else "No Name"
